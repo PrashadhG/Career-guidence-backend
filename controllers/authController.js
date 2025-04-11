379924ce438dbf53df3fb276cdcb56266a1a9304
PrashadhG <prashadh.g.p@gmail.com>
2025-04-11 12:10:05 +0530

    Auth Validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -49,3 +49,13 @@ exports.loginUser = async (req, res) => {
         res.status(500).send("Server Error");
     }
 };
+    
+exports.verifyToken = async (req, res) => {
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      res.json(user);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { registerUser, loginUser } = require("../controllers/authController");
+const { registerUser, loginUser, verifyToken } = require("../controllers/authController");
+const auth = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/verify", auth, verifyToken); 
 
 module.exports = router;
